4529a2d30ce742fdf6fc547a7256a3424a6a8622
0kron <88357657+0kron@users.noreply.github.com>
2021-12-30 19:05:07 -0600

    Delete Functions directory

--- Functions/anagrams.py
+++
@@ -1,8 +0,0 @@
-def anagrams(word, words):
-    isit = True
-    i = 0
-    if len(word) == len(words): 
-        for i in word: 
-            if word.count(i)!=words.count(i): isit = False
-    else: False
-    return isit
